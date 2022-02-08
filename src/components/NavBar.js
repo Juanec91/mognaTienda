55eb5e1722e5689f7e4b42d3c0881efab2df4c5d
Juan <juanchicaval@gmail.com>
2022-02-08 20:39:39 -0300

    segundo entregable

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './NavBar.css'
+
+const NavBar = () => {
+
+
+
+    return(
+
+   
+<nav className="container">
+    <h2 className="title">Mogna</h2>
+    <button className="button-test">Productos</button>
+    <button className="button-test"> Nosotros</button>
+</nav>
+
+    )
+}
+
+
+export default NavBar;
